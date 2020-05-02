f190402b068e9819cb120650998ba1043c7399be
李昌南 <changnan.li@bintools.cn>
2020-05-03 04:35:31 +0800

    创建react分支

--- packages/react/src/ReactNoopUpdateQueue.js
+++ packages/react/src/ReactNoopUpdateQueue.js
@@ -19,9 +19,9 @@ function warnNoop(publicInstance, callerName) {
     }
     console.error(
       "Can't call %s on a component that is not yet mounted. " +
-        'This is a no-op, but it might indicate a bug in your application. ' +
-        'Instead, assign to `this.state` directly or define a `state = {};` ' +
-        'class property with the desired state in the %s component.',
+      'This is a no-op, but it might indicate a bug in your application. ' +
+      'Instead, assign to `this.state` directly or define a `state = {};` ' +
+      'class property with the desired state in the %s component.',
       callerName,
       componentName,
     );
@@ -40,7 +40,7 @@ const ReactNoopUpdateQueue = {
    * @protected
    * @final
    */
-  isMounted: function(publicInstance) {
+  isMounted: function (publicInstance) {
     return false;
   },
 
@@ -59,7 +59,7 @@ const ReactNoopUpdateQueue = {
    * @param {?string} callerName name of the calling function in the public API.
    * @internal
    */
-  enqueueForceUpdate: function(publicInstance, callback, callerName) {
+  enqueueForceUpdate: function (publicInstance, callback, callerName) {
     warnNoop(publicInstance, 'forceUpdate');
   },
 
@@ -76,7 +76,7 @@ const ReactNoopUpdateQueue = {
    * @param {?string} callerName name of the calling function in the public API.
    * @internal
    */
-  enqueueReplaceState: function(
+  enqueueReplaceState: function (
     publicInstance,
     completeState,
     callback,
@@ -97,12 +97,13 @@ const ReactNoopUpdateQueue = {
    * @param {?string} Name of the calling function in the public API.
    * @internal
    */
-  enqueueSetState: function(
+  enqueueSetState: function (
     publicInstance,
     partialState,
     callback,
     callerName,
   ) {
+    debugger
     warnNoop(publicInstance, 'setState');
   },
 };
